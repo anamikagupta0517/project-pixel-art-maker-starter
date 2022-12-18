4fee1572f90cad16c35c669d4994c673a70bcafc
Anamika Gupta <anamikagupta17@gmail.com>
2022-12-17 23:01:52 -0500

    Tried adding row and cells to table

--- designs.js
+++ designs.js
@@ -3,8 +3,31 @@
 
 // When size is submitted by the user, call makeGrid()
 
+const submitButton = document.querySelector('input[type="submit"]');
+
+
+submitButton?.addEventListener('click', (e) => {
+    makeGrid();
+})
+
+
 function makeGrid() {
+    var height = document.querySelector("#inputHeight").value;
+    var width = document.querySelector("#inputWidth").value;
+    
+    var table = document.querySelector("#pixelCanvas");
+    
 
-// Your code goes here!
-
+    for (row = 0; row < height; row++) {
+        var tabRow = document.createElement("TR");
+        tabRow.setAttribute("id", row);
+        table.appendChild(tabRow);
+        for (cell = 0; cell < width; cell++) {
+            var tabCell = document.createElement("TD");
+            tabCell.setAttribute("id", row + cell);
+            tabRow.appendChild(tabCell);
+        }
+        
+    }
 }
+
