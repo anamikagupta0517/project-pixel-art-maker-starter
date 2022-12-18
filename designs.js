89b7680b80cc0aa49c7a32a5ba4708be0a3eba95
Anamika Gupta <anamikagupta17@gmail.com>
2022-12-18 14:35:29 -0500

    completed project

--- designs.js
+++ designs.js
@@ -4,9 +4,16 @@
 // When size is submitted by the user, call makeGrid()
 
 const submitButton = document.querySelector('input[type="submit"]');
+const table = document.querySelector("#pixelCanvas");
+let color = document.getElementById("colorPicker").value;
 
+
+document.getElementById("colorPicker").addEventListener('change', (e) => {
+    color = e.target.value;
+});
 
 submitButton?.addEventListener('click', (e) => {
+    e.preventDefault();
     makeGrid();
 })
 
@@ -14,20 +21,23 @@ submitButton?.addEventListener('click', (e) => {
 function makeGrid() {
     var height = document.querySelector("#inputHeight").value;
     var width = document.querySelector("#inputWidth").value;
-    
-    var table = document.querySelector("#pixelCanvas");
-    
-
-    for (row = 0; row < height; row++) {
+   
+    for (var row = 0; row < height; row++) {
         var tabRow = document.createElement("TR");
         tabRow.setAttribute("id", row);
         table.appendChild(tabRow);
-        for (cell = 0; cell < width; cell++) {
+        for (var cell = 0; cell < width; cell++) {
             var tabCell = document.createElement("TD");
-            tabCell.setAttribute("id", row + cell);
+            tabCell.setAttribute("id", String(row) + String(cell));
             tabRow.appendChild(tabCell);
         }
-        
     }
+    return table;
 }
+
+const onClick = (event) => {
+    event.srcElement.style.backgroundColor = color;
+}
+table.addEventListener('click', onClick);
+
 
